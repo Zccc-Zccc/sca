663ac7a16748c87d1175544bd757d1fdc09030a9
Zccc-Zccc <164682506@qq.com>
2022-05-27 09:16:29 +0800

    update cluster

--- cluster.py
+++ cluster.py
@@ -6,15 +6,30 @@ import codecs
 corpus = []
 with codecs.open("./new_test.csv","r","utf-8") as f:
     corpus = f.readlines()
-km = KMeans(n_clusters=2)
+km = KMeans(n_clusters=10)
 pca = PCA(n_components=2)
 
-vectors =  np.loadtxt("text_vectors.txt")
+vectors =  np.loadtxt("test_vectors.txt")
 vectors_ = pca.fit_transform(vectors)   #降维到二维
 y_ = km.fit_predict(vectors_)       #聚类
 plt.rcParams['font.sans-serif'] = ['FangSong']
 plt.scatter(vectors_[:,0],vectors_[:, 1],c=y_)   #将点画在图上
 for i in range(len(corpus)):    #给每个点进行标注
-    plt.annotate(s=corpus[i], xy=(vectors_[:, 0][i], vectors_[:, 1][i]),
-                 xytext=(vectors_[:, 0][i] + 0.1, vectors_[:, 1][i] + 0.1))
-plt.savefig()
+    # plt.annotate(s=corpus[i], xy=(vectors_[:, 0][i], vectors_[:, 1][i]),
+    #              xytext=(vectors_[:, 0][i] + 0.1, vectors_[:, 1][i] + 0.1))
+    plt.scatter(vectors_[:, 0][i], vectors_[:, 1][i], s = 3)
+plt.savefig(fname = "10")
+plt.cla()
+
+
+vectors =  np.loadtxt("test_vectors_afterNormalize.txt")
+vectors_ = pca.fit_transform(vectors)   #降维到二维
+y_ = km.fit_predict(vectors_)       #聚类
+plt.rcParams['font.sans-serif'] = ['FangSong']
+plt.scatter(vectors_[:,0],vectors_[:, 1],c=y_)   #将点画在图上
+for i in range(len(corpus)):    #给每个点进行标注
+    # plt.annotate(s=corpus[i], xy=(vectors_[:, 0][i], vectors_[:, 1][i]),
+    #              xytext=(vectors_[:, 0][i] + 0.1, vectors_[:, 1][i] + 0.1))
+    plt.scatter(vectors_[:, 0][i], vectors_[:, 1][i], s = 3)
+plt.savefig(fname = "10_afterNormalize")
+plt.cla()
