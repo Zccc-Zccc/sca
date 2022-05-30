6853d88f8c2e40c595b83cbe19f8bfc218926305
Zccc-Zccc <164682506@qq.com>
2022-05-30 18:17:18 +0800

    update

--- dd.py
+++ dd.py
@@ -17,8 +17,8 @@ model_type, pooling  = "BERT" , "cls"
 # assert pooling in ['first-last-avg', 'last-avg', 'cls', 'pooler']
 # assert task_name in ['ATEC', 'BQ', 'LCQMC', 'PAWSX', 'STS-B' , 'myjob']
 
-n_components_ = ["384" , "256"]
-n_components = [384 , 256]
+n_components_ = ["384" , "256" , "768"] 
+n_components = [384 , 256 , 768]
 
 
 maxlen = 64
@@ -34,7 +34,7 @@ task_name = 'dd'
 
 datasets = {
     '%s' % (task_name):
-    load_my_data('./symptom.csv')
+    load_my_data('./new_symptom.csv')
 }
 
 # bert配置
@@ -52,76 +52,66 @@ model_name = {
     'SimBERT-small': 'chinese_simbert_L-6_H-384_A-12'
 }[model_type]
 
-config_path = '/root/kg/bert/%s/bert_config.json' % model_name
+config_path = '/root/kg/skbert/%s/bert_config.json' % model_name
 if model_type == 'NEZHA':
     checkpoint_path = '/root/kg/bert/%s/model.ckpt-691689' % model_name
 elif model_type == 'NEZHA-large':
     checkpoint_path = '/root/kg/bert/%s/model.ckpt-346400' % model_name
 else:
-    checkpoint_path = '/root/kg/bert/%s/bert_model.ckpt' % model_name
-dict_path = '/root/kg/bert/%s/vocab.txt' % model_name
+    checkpoint_path = '/root/kg/skbert/%s/bert_model.ckpt' % model_name
+dict_path = '/root/kg/skbert/%s/vocab.txt' % model_name
 
 # 建立分词器
 tokenizer = get_tokenizer(dict_path)
+poolings = ["cls","first-last-avg","last-avg","pooler"]
+    # 建立模型
+for pooling in poolings:
+    encoder = get_encoder(config_path, checkpoint_path, pooling=pooling)
 
-# 建立模型
-encoder = get_encoder(config_path, checkpoint_path, pooling=pooling)
-
-# 语料向量化
-all_names, all_weights, all_vecs, all_labels = [], [], [], []
-# for name, data in datasets.items():
-#     a_vecs, b_vecs, labels = convert_to_vecs(data, tokenizer, encoder, maxlen)
-#     all_names.append(name)
-#     all_weights.append(len(data))
-#     all_vecs.append((a_vecs, b_vecs))
-#     all_labels.append(labels)
-
-
-########
-for name, data in datasets.items():
-    # print(data)
-    a_vecs = my_convert_to_vecs(data, tokenizer, encoder, maxlen)
-    all_names.append(name)
-    all_weights.append(len(data))
-    all_vecs.append(a_vecs)
+    # 语料向量化
+    all_names, all_weights, all_vecs, all_labels = [], [], [], []
+    # for name, data in datasets.items():
+    #     a_vecs, b_vecs, labels = convert_to_vecs(data, tokenizer, encoder, maxlen)
+    #     all_names.append(name)
+    #     all_weights.append(len(data))
+    #     all_vecs.append((a_vecs, b_vecs))
+    #     all_labels.append(labels)
 
 
-#     # all_labels.append(labels)
+    ########
+    for name, data in datasets.items():
+        # print(data)
+        a_vecs = my_convert_to_vecs(data, tokenizer, encoder, maxlen)
+        all_names.append(name)
+        all_weights.append(len(data))
+        all_vecs.append(a_vecs)
+
+
+    #     # all_labels.append(labels)
 
 
 
-# 计算变换矩阵和偏置项
+    # 计算变换矩阵和偏置项
 
-kernel, bias = compute_kernel_bias([vecs for vecs in all_vecs])
-kernel_384 = kernel[:, :n_components[0]]
-kernel_256 = kernel[:, :n_components[1]]
+    kernel, bias = compute_kernel_bias([vecs for vecs in all_vecs])
+    for c in n_components:
+        kernel = kernel[:, :c]        
+        # 变换，标准化，相似度，相关系数
+        afterNormalize = []
+        transformVec = []
+        for a_vecs in all_vecs:
+            vecs , afterNormalizeVecs = transform_and_normalize(a_vecs, kernel, bias)
+            afterNormalize.append(afterNormalizeVecs)
+            transformVec.append(vecs)
 
-# 变换，标准化，相似度，相关系数
-afterNormalize_384 = []
-afterNormalize_256 = []
-# for (a_vecs, b_vecs), labels in zip(all_vecs, all_labels):
-transformVec_384 = []
-transformVec_256 = []
-for a_vecs in all_vecs:
-    vecs_384 , afterNormalizeVecs_384 = transform_and_normalize(a_vecs, kernel_384, bias)
-    afterNormalize_384.append(afterNormalizeVecs_384)
-    transformVec_384.append(vecs_384)
+            # b_vecs = transform_and_normalize(b_vecs, kernel, bias)
+        #     sims = (a_vecs * b_vecs).sum(axis=1)
+        #     corrcoef = compute_corrcoef(labels, sims)
+        #     all_corrcoefs.append(corrcoef)
+        np.savetxt("./%s/%s/text_vectors_transform.txt"%(str(c) , pooling),afterNormalize[0])
+        np.savetxt("./%s/%s/text_vectors_afterNormalize.txt"%(str(c) , pooling),afterNormalize[0])
+    np.savetxt("./bert/%s/text_vectors_bert.txt" %pooling,all_vecs[0])
 
-    vecs_256 , afterNormalizeVecs_256 = transform_and_normalize(a_vecs, kernel_256, bias)
-    afterNormalize_256.append(afterNormalizeVecs_256)
-    transformVec_256.append(vecs_256)
-    # b_vecs = transform_and_normalize(b_vecs, kernel, bias)
-#     sims = (a_vecs * b_vecs).sum(axis=1)
-#     corrcoef = compute_corrcoef(labels, sims)
-#     all_corrcoefs.append(corrcoef)
-
-np.savetxt("text_vectors_bert.txt",all_vecs[0])
-
-np.savetxt("text_vectors_transform_384.txt",transformVec_384[0])
-np.savetxt("text_vectors_afterNormalize_384.txt",afterNormalize_384[0])
-
-np.savetxt("text_vectors_transform_256.txt",transformVec_256[0])
-np.savetxt("text_vectors_afterNormalize_256.txt",afterNormalize_256[0])
 
 # with codecs.open("./afterWhitening.csv", 'w','utf-8') as f:
 #     for line in all_vecs:
--- dd_cluster.py
+++ dd_cluster.py
@@ -1,25 +1,39 @@
 import matplotlib.pyplot as plt
+from sklearn import cluster
 from sklearn.cluster import KMeans
 import numpy as np
 from sklearn.decomposition import PCA
 import codecs
 corpus = []
-with codecs.open("./new_text.csv","r","utf-8") as f:
+with codecs.open("./new_syptom.csv","r","utf-8") as f:
     corpus = f.readlines()
-km = KMeans(n_clusters=10)
+clusters = 10
+km = KMeans(n_clusters=clusters)
 pca = PCA(n_components=2)
 
-filename = ["text_vectors_bert","text_vectors_transform_384","text_vectors_afterNormalize_384","text_vectors_transform_256","text_vectors_afterNormalize_256"]
-
-for file in filename:
-    vectors =  np.loadtxt("%s.txt" %file)
-    vectors_ = pca.fit_transform(vectors)   #降维到二维
-    y_ = km.fit_predict(vectors_)       #聚类
-    plt.rcParams['font.sans-serif'] = ['FangSong']
-    plt.scatter(vectors_[:,0],vectors_[:, 1],s = 3 , c=y_)   #将点画在图上
-    # for i in range(len(corpus)):    #给每个点进行标注
-    #     # plt.annotate(s=corpus[i], xy=(vectors_[:, 0][i], vectors_[:, 1][i]),
-    #     #              xytext=(vectors_[:, 0][i] + 0.1, vectors_[:, 1][i] + 0.1))
-    #     plt.scatter(vectors_[:, 0][i], vectors_[:, 1][i], s = 3)
-    plt.savefig(fname = ("5_%s" %file))
-    plt.cla()
+filename = ["text_vectors_transform","text_vectors_afterNormalize"]
+components = [384 , 256 , 768]
+poolings = ['first-last-avg', 'last-avg', 'cls', 'pooler']
+for c in components:
+    for pooling in poolings:
+        for file in filename:
+            vectors =  np.loadtxt("./%s/%s/%s.txt" %(str(c), pooling ,file))
+            vectors_ = pca.fit_transform(vectors)   #降维到二维
+            y_ = km.fit_predict(vectors_)       #聚类
+            label = km.labels_
+            # plt.rcParams['font.sans-serif'] = ['FangSong']
+            # plt.scatter(vectors_[:,0],vectors_[:, 1],s = 3 , c=y_)   #将点画在图上
+            for i in range(len(corpus)):    #给每个点进行标注
+                # plt.annotate(s=corpus[i], xy=(vectors_[:, 0][i], vectors_[:, 1][i]),
+                #              xytext=(vectors_[:, 0][i] + 0.1, vectors_[:, 1][i] + 0.1))
+                f = open('./%s/%s/%s/label_%s'  %(str(c), pooling , file ,str(label[i])) , 'a')
+                f.write(corpus[i])
+                f.close
+            plt.rcParams['font.sans-serif'] = ['FangSong']
+            plt.scatter(vectors_[:,0],vectors_[:, 1],s = 3 , c=y_)   #将点画在图上
+            # for i in range(len(corpus)):    #给每个点进行标注
+            #     # plt.annotate(s=corpus[i], xy=(vectors_[:, 0][i], vectors_[:, 1][i]),
+            #     #              xytext=(vectors_[:, 0][i] + 0.1, vectors_[:, 1][i] + 0.1))
+            #     plt.scatter(vectors_[:, 0][i], vectors_[:, 1][i], s = 3)
+            plt.savefig(fname = ("./%s/%s/%s_%s" %(str(c), pooling, clusters ,file)))
+            plt.cla()
